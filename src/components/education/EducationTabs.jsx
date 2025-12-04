8c3fd093eda2384cc6c67ba004f746857aed1b2d
Mohammed Taysir <medo-11223344@hotmail.com>
2025-12-04 10:20:08 +0200

    Updating Portfolio cv and education

--- src/components/education/EducationTabs.jsx
+++ src/components/education/EducationTabs.jsx
@@ -19,8 +19,8 @@ const steps = [
     {
         id: 1,
         title: 'Bachelor of Computer Systems Engineering',
-        date: '📆 From 2022 to 2027', 
-        description: 'Iam A Third Level Student In Alazhar University Gaza'
+        date: '📆 From 2023 to 2027', 
+        description: 'Studying Computer Systems Engineering with a focus on Software Engineering, gaining solid knowledge in software development, system architecture, databases, and networking. Experienced in working on practical projects and applying problem-solving and analytical skills to build efficient technical solutions.'
     },
     {
         id: 2,
@@ -33,9 +33,15 @@ for two years to enhance English language skills.`
         id: 3,
         title: 'Frontend Develpment Course',
         date: '📆 2025',
-        description: `Completed a six-month intensive course covering HTML, CSS, JavaScript, Bootstrap, React, and MUI. Gained hands-on experience in building responsive, interactive, and user-friendly web interfaces.`
+        description: `Completed an intensive 8-month Front-End Development program at Knowledge Academy, covering HTML, CSS, JavaScript, Bootstrap, React, MUI, TypeScript, and Next.js. Developed multiple high-quality projects that enhanced my practical skills in creating responsive, scalable, and user-friendly web interfaces.`
     },
-    
+    {
+        id: 4,
+        title: 'Back-End Development Course – ASP.NET Core | Knowledge Academy',
+        date: '📆 2025',
+        description: `Currently enrolled in a Back-End ASP.NET Core course at Knowledge Academy, focusing on server-side development, API building, authentication, and database integration. This program strengthens my understanding of modern back-end architectures and enhances my full-stack development skills.`
+    },
+
 
 ]
 
@@ -48,7 +54,7 @@ const certifications = [
 ]
 
 function EducationTabs() {
-  
+
     const theme = useTheme();
     const [value, setValue] = useState(0)
     const MotionBox = motion(Box);
@@ -56,90 +62,90 @@ function EducationTabs() {
     return (
         <Box
         >
-            <Stack direction={'row'}  sx = {{
+            <Stack direction={'row'} sx={{
                 p: '5px',
                 bgcolor: theme.palette.background.paper,
                 borderRadius: 2,
-                width: small? '200px': '400px',
+                width: small ? '200px' : '400px',
                 m: 'auto'
             }}>
 
-                <Box flexGrow = {1} >
-                    <Button onClick={() => {setValue(0)}} fullWidth  variant = 'contained' sx = {{
+                <Box flexGrow={1} >
+                    <Button onClick={() => { setValue(0) }} fullWidth variant='contained' sx={{
                         textTransform: 'capitalize',
-                        bgcolor: value == 0? theme.palette.background.default : 'transparent',
+                        bgcolor: value == 0 ? theme.palette.background.default : 'transparent',
                         boxShadow: 0,
-                        
+
                         color: theme.palette.text.primary,
                         fontWeight: 'normal',
                         ":hover": {
                             boxShadow: 0
                         }
-                    }} size = 'small' >Education</Button>
+                    }} size='small' >Education</Button>
                 </Box>
-                
-                <Box flexGrow = {1}>
-                    <Button onClick={() => {setValue(1)}} fullWidth variant = 'contained' sx = {{
+
+                <Box flexGrow={1}>
+                    <Button onClick={() => { setValue(1) }} fullWidth variant='contained' sx={{
                         textTransform: 'capitalize',
-                        bgcolor: value == 1? theme.palette.background.default : 'transparent',
+                        bgcolor: value == 1 ? theme.palette.background.default : 'transparent',
                         boxShadow: 0,
                         color: theme.palette.text.primary,
                         fontWeight: 'normal',
                         ":hover": {
                             boxShadow: 0
                         }
-                    }} size = 'small' >Certificates</Button>
+                    }} size='small' >Certificates</Button>
                 </Box>
-                
+
             </Stack>
 
-            {value == 0?(
-            <MotionBox
-            initial = {{y: 100, opacity: 0}}
-            whileInView={{y: 0, opacity: 1}}
-            transition={{
-                duration: 0.5
-            }}>
-                <Box sx = {{
-                    display: 'grid',
-                    gap: '20px',
-                    mt: 5,
-                    gridTemplateColumns: 'repeat(auto-fit, minmax(200px, 1fr))'
+            {value == 0 ? (
+                <MotionBox
+                    initial={{ y: 100, opacity: 0 }}
+                    whileInView={{ y: 0, opacity: 1 }}
+                    transition={{
+                        duration: 0.5
+                    }}>
+                    <Box sx={{
+                        display: 'grid',
+                        gap: '20px',
+                        mt: 5,
+                        gridTemplateColumns: 'repeat(auto-fit, minmax(300px, 1fr))'
+                    }}>
+
+                        {
+                            steps.map(step => (<Step key={step.id} step={step} />))
+                        }
+
+                    </Box>
+                </MotionBox>
+            ) : (<MotionBox
+                initial={{ y: 100, opacity: 0 }}
+                whileInView={{ y: 0, opacity: 1 }}
+                transition={{
+                    duration: 0.5
+                }}
+            >
+
+                <Box sx={{
+
+                    mt: 5
                 }}>
 
-                    {
-                        steps.map(step => (<Step key = {step.id} step = {step} />))
-                    }
+                    <CustomSwiper>
 
+                        {
+                            certifications.map(cer => (<SwiperSlide key={cer.id}>
+                                <Cer cer={cer} />
+                            </SwiperSlide>))
+                        }
+
+                    </CustomSwiper>
                 </Box>
-            </MotionBox>
-        ): (<MotionBox
-            initial = {{y: 100, opacity: 0}}
-            whileInView={{y: 0, opacity: 1}}
-            transition = {{
-                duration: 0.5
-            }}
-        >
-
-            <Box sx = {{
-
-                mt: 5
-            }}>
-
-                <CustomSwiper>
-
-                    {
-                        certifications.map(cer => (<SwiperSlide key = {cer.id}>
-                            <Cer cer = {cer} />    
-                        </SwiperSlide>))
-                    }
-
-                </CustomSwiper>
-            </Box>
-        </MotionBox>)}
+            </MotionBox>)}
 
         </Box>
-        
+
     );
 }
 
